03d7141602be3416ff8d25f3401ec0c6e050a6df
marius0411 <constantinm42@yahoo.com>
2019-07-13 13:30:54 +0300

    jhfududt

---
+++ src/Executie.java
@@ -0,0 +1,12 @@
+import entitati.Om;
+
+public class Executie {
+
+
+    public static void main(String args[]) {
+
+        Om x = new Om();
+        x.setNume("jfghjfghhj");
+        x.afisareInfoOm(x);
+    }
+}
--- src/entitati/Om.java
+++ src/entitati/Om.java
@@ -1,4 +1,34 @@
 package entitati;
 
 public class Om {
+
+    int varsta;
+    String nume, prenume;
+    double inaltime;
+
+    public void setVarsta(int v) {
+        this.varsta = v;
+    }
+
+    public void setNume(String n) {
+        this.nume = n;
+    }
+
+    public void setPrenume(String p) {
+        this.prenume = p;
+    }
+
+    public void setInaltime(double i) {
+        this.inaltime = i;
+    }
+
+
+    public void afisareInfoOm(Om om) {
+
+        System.out.println("omul nostru are numele " + this.nume);
+
+
+    }
+
 }
+
